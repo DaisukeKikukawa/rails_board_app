2047e21302a816cde27b6b3a0ba9ada3bc704acd
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-04-12 10:33:07 +0900

    認証機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # get 'static_pages/top'
   root :to => 'static_pages#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/login' , to: 'user_sessions#new'
+  post '/login' , to: 'user_sessions#create'
+  delete '/logout' , to: 'user_sessions#destroy'
 end
