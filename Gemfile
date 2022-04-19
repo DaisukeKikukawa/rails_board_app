f6c28fcd5a82d5efb462888d163c610f080bea09
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-04-19 16:34:14 +0900

    掲示板作成機能の追加

--- Gemfile
+++ Gemfile
@@ -70,5 +70,5 @@ gem 'jquery-rails'
 gem 'pry-byebug'
 gem 'sorcery', '~> 0.14.0'
 
-gem 'rails-i18n'
+gem 'rails-i18n', '5.1.3'
 gem 'draper'
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module RunteqNormal
     config.load_defaults 5.2
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.assets false
